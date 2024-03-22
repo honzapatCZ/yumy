eabfb65b3cedec563e12b492405efd1f5329d0bb
Jan Patrovský <honzapatCZ@email.cz>
2024-03-22 09:43:14 +0100

    update splash

--- layers/meta-yummy/recipes-core/psplash/files/psplash-colors.h
+++ layers/meta-yummy/recipes-core/psplash/files/psplash-colors.h
@@ -18,7 +18,7 @@
 #define PSPLASH_TEXT_COLOR 0x6d,0x6d,0x70
 
 /* This is the color of the progress bar indicator */
-#define PSPLASH_BAR_COLOR 0xec,0xec,0xe1
+#define PSPLASH_BAR_COLOR 0xff,0xff,0xff
 
 /* This is the color of the progress bar background */
 #define PSPLASH_BAR_BACKGROUND_COLOR 0x00,0x00,0x00
